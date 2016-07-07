407d79384a5dd8fb7f8422cf8bf7f39197db2177
Michael Miller <mmiller@systemsbiology.org>
2016-07-07 13:47:33 -0700

    make metadata_tables OrderedDict for all  isbcgc_cloudsql_model.ISBCGC_database_helper subclasses

--- data_upload/model/isbcgc_cloudsql_core_model.py
+++ data_upload/model/isbcgc_cloudsql_core_model.py
@@ -21,6 +21,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 import isbcgc_cloudsql_model
+from collections import OrderedDict
 
 class ISBCGC_database_helper(isbcgc_cloudsql_model.ISBCGC_database_helper):
     """
@@ -502,12 +503,14 @@ class ISBCGC_database_helper(isbcgc_cloudsql_model.ISBCGC_database_helper):
         ]
     }
     
-    isbcgc_cloudsql_model.ISBCGC_database_helper.metadata_tables = {
-        'metadata_clinical': metadata_clinical,
-        'metadata_biospecimen': metadata_biospecimen,
-        'metadata_data': metadata_data,
-        'metadata_samples': metadata_samples
-    }
+    isbcgc_cloudsql_model.ISBCGC_database_helper.metadata_tables = OrderedDict(
+        [
+            ('metadata_clinical', metadata_clinical),
+            ('metadata_biospecimen', metadata_biospecimen),
+            ('metadata_data', metadata_data),
+            ('metadata_samples', metadata_samples)
+        ]
+    )
 
     self = None
 
