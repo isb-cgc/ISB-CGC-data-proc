5b0c2ef1ea65b4f9d7aeaa401304fef11f7289bb
Michael Miller <mmiller@systemsbiology.org>
2017-05-16 16:23:52 -0700

    modified the ogic to work up from TCGA_metadata_clincial to TCGA_metadata_data_HG19 so records are in place

--- gdc/util/process_images.py
+++ gdc/util/process_images.py
@@ -21,49 +21,71 @@ from bq_wrapper import fetch_paged_results, query_bq_table
 from isbcgc_cloudsql_model import ISBCGC_database_helper
 from util import print_list_synopsis
 
-def process_image_updates(config, image_config, rows, log):
+def process_data_image_records(config, image_config, image_type, rows, log):
     '''
-    based on the config maps, checks to see if the config-specified metadata data table can be updated with the GCS file path
-    for the sample, file_name combination for each of the rows that has a match
+    based on either the case_barcode (for radiology images) or the sample_barcode (for tissue or diagnostic images),
+    either updates or creates a new metadata data record in the config-specified metadata data table
     
     parameters:
         config: configuration mappings
         image_config: section of the config file with specific mappings for these image types
+        not_barcodes: an output parameter for barcodes that weren't found in the underlying clinical or biospecimen table
         rows: rows selected from the BigQuery table
         log: where to write progress and other messages
-    
-    returns:
-        rows: rows that weren't mapped to existing rows from BigQuery 
+
+        file_gdc_id: ?
+        case_gdc_id: ?
+        case_barcode: row[2]
+        sample_gdc_id: ?
+        sample_barcode: row[0]
+        sample_type: sample_barcode[13:15]
+        aliquot_barcode: row[3]
+        aliquot_gdc_id: ?
+        project_short_name: row[4].split('/')[6]
+        disease_code: project_short_name.split('-')[1]
+        program_name: 'TCGA'
+        data_type: image_type
+        data_category: 'Clinical'
+        type: 'file'
+        file_name: row[1]
+        data_format: 'SVS'
+        access: 'open'
+        acl: 'open'
+        platform: 'Clinical'
+        file_name_key: row[4]
+        file_uploaded: 'true'
+        endpoint_type: 'legacy'
+        species: 'Homo sapien'
     '''
-    data_columns = image_config['data_columns'] if 'data_columns' in image_config else None
-    data_select_template = image_config['data_select_template'].format(','.join(data_columns)) if 'data_select_template' in image_config else None
-    if data_select_template:
-        # select matching rows from the metadata data for the bigquery rows
-        data_select = data_select_template % (','.join(["'" + bq_row[0] + "'" for bq_row in rows]))
-        data_rows = ISBCGC_database_helper.select(config, data_select, log, [])
-        
-        # get the parameters from row info from the bigquery select for the update
-        data_rows_map = {tuple(row):None for row in data_rows}
-        update_rows = [row for row in rows if (row[0], row[1]) in data_rows_map]
-        log.info('\t\tfound %d row(s) for updating file_name_key after filtering' % (len(update_rows)))
-        updates = []
-        for update_row in update_rows:
-            update = []
-            for update_column in image_config['update_columns']:
-                update += [update_row[update_column]]
-            
-            updates += [update]
-        
-        # do the update
-        ISBCGC_database_helper.update(config, image_config['data_update_template'], log, updates)
-        
-        # get the bigquery rows that weren't updated and return them
-        rows = [row for row in rows if (row[0], row[1]) not in data_rows_map]
-        log.info('\t\t%d row(s) to insert after filtering' % (len(rows)))
-    return rows
-
-
-def process_image_inserts(config, image_config, not_barcodes, rows, log):
+    # get the information from the config mapping
+    log.info('\tchecking data records')
+    barcode2row = dict([(row[0], row) for row in rows])
+    data_select_template = image_config['data_select_template']
+    data_rows = ISBCGC_database_helper.select(config, data_select_template % ("'" + "','".join(barcode2row) + "'"), log, [])
+    barcode_db = set([data_row[0] for data_row in data_rows])
+    new_barcodes = set(barcode2row) - barcode_db
+    if 0 < len(new_barcodes):
+        log.info('\t\tinserting {} new data records'.format(len(new_barcodes)))
+        rows = []
+        for barcode in new_barcodes:
+            row = barcode2row[barcode]
+            project_short_name = row[4].split('/')[6]
+            rows += [[row[2], row[0], row[0][13:15], row[3], row[4].split('/')[6], project_short_name.split('-')[1], 'TCGA', config['image_type2data_type']['image_type'], 
+                      'Clinical', 'file', row[1], 'SVS', 'open', 'open', 'Clinical', row[4], 'true', 'legacy', 'Homo sapien']]
+        ISBCGC_database_helper.column_insert(config, rows, image_config['data_table'], image_config['data_columns'], log)
+    else:
+        log.info('\t\tno rows to insert for biospecimen records')
+    if 0 < len(barcode_db):
+        log.info('\t\tupdating {} existing data records'.format(len(barcode_db)))
+        rows = []
+        for barcode in new_barcodes:
+            row = barcode2row[barcode]
+            rows += [[row[4], row[0], row[row[1]]]]
+            ISBCGC_database_helper.update(config, image_config['data_update_template'], log, rows)
+    else:
+        log.info('\t\tno rows to update for biospecimen records')
+    
+def process_sanple_image_records(config, image_config, rows, log):
     '''
     based on either the case_barcode (for radiology images) or the sample_barcode (for tissue or diagnostic images),
     creates a new metadata data record in the config-specified metadata data table
@@ -74,52 +96,134 @@ def process_image_inserts(config, image_config, not_barcodes, rows, log):
         not_barcodes: an output parameter for barcodes that weren't found in the underlying clinical or biospecimen table
         rows: rows selected from the BigQuery table
         log: where to write progress and other messages
+
+        endpoint_type: 'legacy'
+        sample_gdc_id: ?
+        sample_barcode: row[0]
+        case_gdc_id: ?
+        case_barcode: row[2]
+        program_name: 'TCGA'
+        disease_code: project_name.split('-')[1]
+        project_short_name: row[4].split('/')[6]
+    '''
+    log.info('\tchecking samples records')
+    barcode2row = dict([(row[0], row) for row in rows])
+    samples_select_template = image_config['samples_select_template']
+    samples_rows = ISBCGC_database_helper.select(config, samples_select_template % ("'" + "','".join(barcode2row) + "'"), log, [])
+    barcode_db = set([samples_row[0] for samples_row in samples_rows])
+    new_barcodes = set(barcode2row) - barcode_db
+    if 0 < len(new_barcodes):
+        log.info('\t\tinserting {} new samples records'.format(len(new_barcodes)))
+        rows = []
+        for barcode in new_barcodes:
+            row = barcode2row[barcode]
+            project_short_name = row[4].split('/')[6]
+            rows += [['current', row[0], row[0][13:15], row[2], 'TCGA', project_short_name.split('-')[1], project_short_name]]
+        ISBCGC_database_helper.column_insert(config, rows, image_config['samples_table'], image_config['biospecimen_columns'], log)
+    else:
+        log.info('\t\tno rows to insert for samples records')
+
+def process_biospecimen_image_records(config, image_config, rows, log):
+    '''
+    based on either the sample_barcode (for tissue or diagnostic images),
+    creates a new metadata clinical record if necessary in the config-specified metadata clinical table
+    
+    parameters:
+        config: configuration mappings
+        image_config: section of the config file with specific mappings for these image types
+        rows: rows selected from the BigQuery table
+        log: where to write progress and other messages
+
+        endpoint_type: 'legacy'
+        sample_gdc_id: ?
+        sample_barcode: row[0]
+        sample_type: row[0][13:15]
+        case_gdc_id: ?
+        case_barcode: row[2]
+        program_name: 'TCGA'
+        disease_code: project_name.split('-')[1]
+        project_short_name: row[4].split('/')[6]
     '''
     # get the information from the config mapping
-    clinical_columns = image_config['clinical_columns']
-    clinical_select_template = image_config['clinical_select_template'].format(','.join(clinical_columns))
-    file_columns = image_config['file_columns']
-    file_column_names = sorted(file_columns.keys())
-    literal_columns = image_config['literal_columns']
-    literal_columns_names = sorted(literal_columns.keys())
-    literal_values = [literal_columns[column] for column in literal_columns_names]
-    insert_columns = clinical_columns + file_column_names + literal_columns_names
-    barcode_index = image_config['barcode_column_index']
+    log.info('\tchecking biospecimen records')
+    barcode2row = dict([(row[0], row) for row in rows])
+    biospecimen_select_template = image_config['biospecimen_select_template']
+    biospecimen_rows = ISBCGC_database_helper.select(config, biospecimen_select_template % ("'" + "','".join(barcode2row) + "'"), log, [])
+    barcode_db = set([biospecimen_row[0] for biospecimen_row in biospecimen_rows])
+    new_barcodes = set(barcode2row) - barcode_db
+    if 0 < len(new_barcodes):
+        log.info('\t\tinserting {} new biospecimen records'.format(len(new_barcodes)))
+        rows = []
+        for barcode in new_barcodes:
+            row = barcode2row[barcode]
+            project_short_name = row[4].split('/')[6]
+            rows += [['legacy', row[0], row[0][13:15], row[2], 'TCGA', project_short_name.split('-')[1], project_short_name]]
+            rows += [['current', row[0], row[0][13:15], row[2], 'TCGA', project_short_name.split('-')[1], project_short_name]]
+        ISBCGC_database_helper.column_insert(config, rows, image_config['biospecimen_table'], image_config['biospecimen_columns'], log)
+    else:
+        log.info('\t\tno rows to insert for biospecimen records')
 
-    if 0 < len(rows):
-        # select info from the metadata clinical table (for radiology) or the biospecimen (for tissue or diagnostic)
-        clinical_select = clinical_select_template % (','.join(["'" + bq_row[0] + "'" for bq_row in rows]))
-        clinical_rows = ISBCGC_database_helper.select(config, clinical_select, log, [])
-        barcode2info = {}
-        for row in clinical_rows:
-            barcode2info[row[barcode_index]] = row
+def process_clinical_image_records(config, image_config, rows, log):
+    '''
+    based on either the case_barcode (for radiology images) or the sample_barcode (for tissue or diagnostic images),
+    creates a new metadata data record in the config-specified metadata data table
     
-    insert_rows = []
-    for bq_row in rows:
-        # match up the rows that exist in BigQuery table that are in the database 
-        if bq_row[0] not in barcode2info:
-            not_barcodes.add(bq_row[0])
-        else:
-            file_values = []
-            for column in file_column_names:
-                fields = file_columns[column].split(':')
-                if 1 == len(fields):
-                    file_values += [bq_row[int(fields[0])]]
-                elif 'split' == fields[1]:
-                    file_values += [bq_row[int(fields[0])].split(fields[2])[int(fields[3])]]
-                else:
-                    raise ValueError('unknown operation: %s' % (fields))
-            
-            insert_rows += [list(barcode2info[bq_row[0]]) + file_values + literal_values]
-    
-    print_list_synopsis(insert_rows, '%s:\n%s' % (image_config['data_insert_table'], insert_columns), log, 1)
-    if 0 < len(insert_rows):
-        # insert the matching rows in the metadata data table
-        ISBCGC_database_helper.column_insert(config, insert_rows, image_config['data_insert_table'], insert_columns, log)
+    parameters:
+        config: configuration mappings
+        image_config: section of the config file with specific mappings for these image types
+        not_barcodes: an output parameter for barcodes that weren't found in the underlying clinical or biospecimen table
+        rows: rows selected from the BigQuery table
+        log: where to write progress and other messages
 
+        endpoint_type: 'legacy'
+        case_gdc_id: ?
+        case_barcode: row[2]
+        program_name: 'TCGA'
+        disease_code: project_name.split('-')[1]
+        project_short_name: row[4].split('/')[6]
+    '''
+    # get the information from the config mapping
+    log.info('\tchecking clinical records')
+    barcode2row = dict([(row[2], row) for row in rows])
+    clinical_select_template = image_config['clinical_select_template']
+    clinical_rows = ISBCGC_database_helper.select(config, clinical_select_template % ("'" + "','".join(barcode2row) + "'"), log, [])
+    barcode_db = set([clinical_row[0] for clinical_row in clinical_rows])
+    new_barcodes = set(barcode2row) - barcode_db
+    if 0 < len(new_barcodes):
+        log.info('\t\tinserting {} new clinical records'.format(len(new_barcodes)))
+        
+        rows = []
+        for barcode in new_barcodes:
+            row = barcode2row[barcode]
+            project_short_name = row[4].split('/')[6]
+            rows += [['legacy', row[2], 'TCGA', project_short_name.split('-')[1], project_short_name]]
+            rows += [['current', row[2], 'TCGA', project_short_name.split('-')[1], project_short_name]]
+        ISBCGC_database_helper.column_insert(config, rows, image_config['clinical_table'], image_config['clinical_columns'], log)
+    else:
+        log.info('\t\tno rows to insert for clinical records')
+
+def process_image_records(config, image_config, rows, log):
+    '''
+    based on either the case_barcode (for radiology images) or the sample_barcode (for tissue or diagnostic images),
+    processes the BigQuery record to make sure there is a clinical, biospecimen (and samples), and data record for
+    that image
+    
+    parameters:
+        config: configuration mappings
+        image_config: section of the config file with specific mappings for these image types
+        rows: rows selected from the BigQuery table
+        log: where to write progress and other messages
+
+    '''
+    # call through each of the tables that have potential inserts or updates
+    process_clinical_image_records(config, image_config, rows, log)
+    process_biospecimen_image_records(config, image_config, rows, log)
+    process_sanple_image_records(config, image_config, rows, log)
+    process_data_image_records(config, image_config, rows, log)
+    
 def process_image_type(config, image_type, log):
     '''
-    based on the configuration map loaded from the configFileName, loads the DCC data into GCS.  also
+    based on the configuration map loaded from the configFileName, loads the DCC metadata into CloudSQL.  also
     obtains metadata based on file paths, SDRF values, and CGHub manifest values
     
     parameters:
@@ -131,8 +235,6 @@ def process_image_type(config, image_type, log):
     for program in programs:
         # for programs with images, select the appropriate section from the config file
         image_config = config[program]['process_files']['images'][image_type]
-        not_barcodes = set()
-
         # query the big query table
         bq_select_template = image_config['bq_select_template']
         bq_columns = image_config['bq_columns']
@@ -142,15 +244,13 @@ def process_image_type(config, image_type, log):
             # loop through the big query results
             total_rows, rows, page_token = fetch_paged_results(query_results, image_config['fetch_count'], None, page_token, log)
             # process updates to the metadata data table
-            rows = process_image_updates(config, image_config, rows, log)
-
+            rows = process_image_records(config, image_config, rows, log)
+    
             # create inserts into the metadata data that for big query rows that didn't have a match already in the metadata data table
-            process_image_inserts(config, image_config, not_barcodes, rows, log)
             if not page_token:
-                log.info('\t\tcreated total of %s rows' % (total_rows))
+                log.info('\t\tchecked total of %s rows' % (total_rows))
                 break
-
-        log.info('\tbig_query %s barcodes(%s) not in the metadata table for %s:\n\t%s\n\t\t...\n%s\n' % (image_type, len(not_barcodes), program, '\n\t'.join(sorted(list(not_barcodes)))[:5], '\n\t'.join(sorted(list(not_barcodes)))[-5:]))
+    
 
 def process_radiology_images(config, log):
     '''
@@ -198,5 +298,5 @@ def process_images(config, log):
     '''
 #     process_radiology_images(config, log)
     process_diagnostic_images(config, log)
-    process_tissue_images(config, log)
+#     process_tissue_images(config, log)
 
