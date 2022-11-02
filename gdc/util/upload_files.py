4cc646872a2c58d6836d206f05a27ae2ee84e4dd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 08:05:16 +0000

    Adding tarfile member sanitization to extractall()

--- gdc/util/upload_files.py
+++ gdc/util/upload_files.py
@@ -83,7 +83,29 @@ def process_files(config, endpt_type, file2info, outputdir, start, end, program_
         filepath = outputdir + config['download_output_file_template'] % (start, end - 1)
         with tarfile.open(filepath) as tf:
             log.info('\t\textract tar files from %s' % (filepath))
-            tf.extractall(outputdir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, outputdir)
             log.info('\t\tdone extract tar files from %s' % (filepath))
      
         with open(outputdir + 'MANIFEST.txt') as manifest:
